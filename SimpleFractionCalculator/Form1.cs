4bc079b0b0197b1542511175f68495ee48035c4f
Mustaine Mustaine <ana-arina@yandex.ru>
2021-06-20 14:24:35 +0300

    Добавьте файлы проекта.

---
+++ SimpleFractionCalculator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFractionCalculator
+{
+    public partial class Form1 : Form
+    {
+        Fraction call;
+        public Form1()
+        {
+            InitializeComponent();
+            call = new Fraction();
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            textBox1.Text = call.handle(text);
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Print example, format 'a/b+c/d', then press '='\nAvailable operations: '+', '-', '*', ':'");
+        }
+    }
+}
---
+++ SimpleFractionCalculator/FractionCalc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace SimpleFractionCalculator
+{
+    class Fraction
+    {
+        private int top;
+        private int bottom;
+
+        public Fraction() { }
+        private Fraction(int a, int b)
+        {
+            if (a == 0) a = 1;
+            if (b == 0) b = 1;
+            top = a; bottom = b;
+        }
+        private Fraction (string s)
+        {
+            string[] elems = s.Split('/');
+            int a = Convert.ToInt32(elems[0]);
+            int b = Convert.ToInt32(elems[1]);
+            if (a == 0) a = 1;
+            if (b == 0) b = 1;
+            top = a; bottom = b;
+        }
+        private void reducate()
+        {
+            int a = top;
+            int b = bottom;
+
+            while (b % a != 0)
+            {
+                a = b - a * (b / a);
+            }
+
+            top /= a;
+            bottom /= a;
+        }
+        private string show()
+        {
+            return top.ToString() + "/" + bottom.ToString();
+        }
+        private Fraction add(Fraction a, Fraction b)
+        {
+            return new Fraction(a.top*b.bottom + b.top*a.bottom, a.bottom * b.bottom);
+        }
+        private Fraction substract(Fraction a, Fraction b)
+        {
+            return new Fraction(a.top * b.bottom - b.top * a.bottom, a.bottom * b.bottom);
+        }
+        private Fraction multiply(Fraction a, Fraction b)
+        {
+            return new Fraction(a.top * b.top, a.bottom * b.bottom);
+        }
+        private Fraction divide(Fraction a, Fraction b)
+        {
+            return new Fraction(a.top * b.bottom, a.bottom * b.top);
+        }
+        public string handle(string s)
+        {
+            char[] operators = new char[] { '+', '-', ':', '*' };
+            string[] temp = s.Split(operators);
+
+            Fraction f1 = new Fraction(temp[0]);
+            Fraction f2 = new Fraction(temp[1]);
+            Fraction res = new Fraction(1, 1);
+
+            char op = ' ';
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '+' || s[i] == '-' || s[i] == ':' || s[i] == '*') op = s[i];
+            }
+
+            switch (op)
+            {
+                case '+': res = add(f1, f2); break;
+                case '-': res = substract(f1, f2); break;
+                case '*': res = multiply(f1, f2); break;
+                case ':': res = divide(f1, f2); break;
+            }
+
+            res.reducate();
+            return res.show();
+        }
+    }
+}
